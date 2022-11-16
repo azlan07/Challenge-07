2bc5aed439a73a8d870ee3682d004c380cb5c704
Azlan <azlan.nur15.07hakim@gmail.com>
2022-11-16 22:29:25 +0700

    eslint delete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react"
-import { Link, useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
 
-    function handleLogout(e) {
+    function handleLogout() {
         localStorage.removeItem("token");
         navigate('/login')
     }
@@ -26,25 +26,25 @@ const Navbar = () => {
         }
     }, [location.pathname]);
     return (
-        <nav class="navbar fixed-top navbar-expand-lg" style={{backgroundColor:"#F1F3FF"}}>
-            <div class="container">
-                <a href="/" class="navbar-brand"><img src="img/logo.png" alt="" /></a>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <div class="navbar-nav ms-auto">
-                        <a class="nav-link active text-dark mx-3 fw-bold" aria-current="page"
+        <nav className="navbar fixed-top navbar-expand-lg" style={{backgroundColor:"#F1F3FF"}}>
+            <div className="container">
+                <a href="/" className="navbar-brand"><img src="img/logo.png" alt="" /></a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="navbar-nav ms-auto">
+                        <a className="nav-link active text-dark mx-3 fw-bold" aria-current="page"
                             href="#our-services">Our
                             Service</a>
-                        <a class="nav-link text-dark mx-3 fw-bold" href="#why-us">Why Us</a>
-                        <a class="nav-link text-dark mx-3 fw-bold" href="#testimonial">Testimonial</a>
-                        <a class="nav-link text-dark mx-3 fw-bold" href="#faq">FAQ</a>
+                        <a className="nav-link text-dark mx-3 fw-bold" href="#why-us">Why Us</a>
+                        <a className="nav-link text-dark mx-3 fw-bold" href="#testimonial">Testimonial</a>
+                        <a className="nav-link text-dark mx-3 fw-bold" href="#faq">FAQ</a>
                         {showButton ? (
-                            <a class="nav-link btn btn-success btn-md text-light mx-3 fw-bold" href="/login">Register</a>
+                            <a className="nav-link btn btn-success btn-md text-light mx-3 fw-bold" href="/login">Register</a>
                         ) : (
-                            <a key={"logout"} class="nav-link btn btn-primary btn-md text-light mx-3 fw-bold" href="/login" onClick={handleLogout}>Logout</a>
+                            <a key={"logout"} className="nav-link btn btn-primary btn-md text-light mx-3 fw-bold" href="/login" onClick={handleLogout}>Logout</a>
                         )}
                     </div>
                 </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <img src="img/fi_menu.png" alt=""/>
                 </button>
